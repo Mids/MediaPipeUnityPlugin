09fe62cccbb9cd4c3f8291252c1a9608681040c0
Jin Park <jinpark.dev@gmail.com>
2022-09-20 15:01:44 +0900

    Tutorial

--- Assets/MediaPipeUnity/Tutorial/Hello World/HelloWorld.cs
+++ Assets/MediaPipeUnity/Tutorial/Hello World/HelloWorld.cs
@@ -10,5 +10,59 @@ using UnityEngine;
 
 namespace Mediapipe.Unity.Tutorial
 {
-  public class HelloWorld : MonoBehaviour { }
+public class HelloWorld : MonoBehaviour
+{
+  private void Start()
+  {
+    var configText = @"
+input_stream: ""in""
+output_stream: ""out""
+node {
+  calculator: ""PassThroughCalculator""
+  input_stream: ""in""
+  output_stream: ""out1""
+}
+node {
+  calculator: ""PassThroughCalculator""
+  input_stream: ""out1""
+  output_stream: ""out""
+}
+";
+
+    Protobuf.SetLogHandler(Protobuf.DefaultLogHandler);
+    var graph = new CalculatorGraph(configText);
+
+    var statusOrPoller = graph.AddOutputStreamPoller<string>("out");
+
+    if (!statusOrPoller.Ok()) return;
+
+    var poller = statusOrPoller.Value();
+
+
+    graph.StartRun().AssertOk();
+
+    for (var i = 0; i < 10; i++)
+    {
+      // Send input to running graph
+      var input = new StringPacket("Hello World!", new Timestamp(i));
+      graph.AddPacketToInputStream("in", input).AssertOk();
+    }
+
+    graph.CloseInputStream("in").AssertOk();
+
+    var output = new StringPacket();
+
+    while (poller.Next(output)) Debug.Log(output.Get());
+
+    graph.WaitUntilDone().AssertOk();
+    graph.Dispose();
+
+    Debug.Log("Done");
+  }
+
+  private void OnApplicationQuit()
+  {
+    Protobuf.ResetLogHandler();
+  }
+}
 }
